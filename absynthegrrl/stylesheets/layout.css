66c82da70d9d39677ef33280b8d5514f8b88ed8b
Arascheya <Arascheya@users.noreply.github.com>
2014-09-28 17:27:36 -0500

    Initial file add

---
+++ absynthegrrl/stylesheets/layout.css
@@ -0,0 +1,172 @@
+body
+{
+    margin: 10 10 10 10;
+}
+
+.navsidebarenvelope
+{
+    width:140px;
+    height: 100%;
+    vertical-align: top;
+    text-align: left;
+}
+
+.navbarbody
+{
+
+}
+
+.navbarcontent
+{
+    font-size: 8pt;
+}
+.navbarcontent_em
+{
+    font-size: 8pt;
+}
+
+.pagecaption
+{
+    font-size: 16pt;
+    margin-left:1.5in;
+}
+
+.pagecapt2
+{
+    font-size: 16pt;
+    text-align: center;
+}
+
+.sectioncaption
+{
+    font-size: 14pt;
+    margin-left:1.5in;
+}
+
+.sectcapt2
+{
+    font-size: 12pt;
+    margin-left:1.5in;
+}
+
+.sectcapt3
+{
+    font-size: 14pt;
+    text-align: center;
+}
+
+.sectcapt4
+{
+    font-size: 14pt;
+    margin-left:1.5in;
+}
+
+.myinfo
+{
+    font-size: 10pt;
+    text-align: right;
+}
+
+.notez
+{
+    font-size: 10pt;
+    text-align: center;
+    padding: 10 20 10 20;
+}
+
+.ptxt
+{
+    font-size: 12pt;
+    text-align: center;
+}
+
+.ptxt2
+{
+    font-size: 12pt;
+    margin-left:1.0in;
+}
+
+.regtxt
+{
+    font-size: 12pt;
+}
+
+.regtxt5
+{
+    font-size: 12pt;
+    margin-left:.5in;
+}
+
+.regtxt9
+{
+    font-size: 12pt;
+    margin-left:.9in;
+}
+
+.regtxt12
+{
+    font-size: 12pt;
+    margin-left:1.2in;
+}
+
+.regtxt15
+{
+    font-size: 12pt;
+    margin-left:1.5in;
+}
+
+.regtxt18
+{
+    font-size: 12pt;
+    margin-left:1.8in;
+}
+
+.regtxt20
+{
+    font-size: 12pt;
+    margin-left:2.0in;
+}
+
+.regtxt23
+{
+    font-size: 12pt;
+    margin-left:2.2in;
+}
+
+.regtxt25
+{
+    font-size: 12pt;
+    margin-left:2.5in;
+}
+
+.regtxt28
+{
+    font-size: 12pt;
+    margin-left:2.8in;
+}
+
+.cpyrt5
+{
+    font-size: 10pt;
+    margin-left:.5in;
+}
+
+.mainmenuitem
+{
+    white-space: nowrap
+    font-size: 10pt;
+    text-align: center;
+    vertical-align: top;
+    padding: 0 10 0 10;
+}
+
+.mainmenuitem_em
+{
+    white-space: nowrap
+    font-size: 10pt;
+    text-align: center;
+    vertical-align: top;
+    padding: 0 10 0 10;
+}
+
+
